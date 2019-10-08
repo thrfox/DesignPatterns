35f5af0e8457f10047ef453ff75f7224758e53ff
mazhuquan_ <www4836@gmail.com>
2019-10-08 10:10:00 +0800

    - 抽象工厂-工厂方法模式

--- AbstractFactoryMode/FactoryMethod/MysqlFactory.java
+++ AbstractFactoryMode/FactoryMethod/MysqlFactory.java
@@ -1,11 +1,18 @@
 package AbstractFactoryMode.FactoryMethod;
 
+import AbstractFactoryMode.Bean.IDepartment;
 import AbstractFactoryMode.Bean.IUser;
+import AbstractFactoryMode.Bean.MysqlDept;
 import AbstractFactoryMode.Bean.MysqlUser;
 
 public class MysqlFactory implements IFactory {
     @Override
     public IUser CreateUser() {
         return new MysqlUser();
+    }
+
+    @Override
+    public IDepartment CreateDepartment() {
+        return new MysqlDept();
     }
 }
--- AbstractFactoryMode/FactoryMethod/SQLServerFactory.java
+++ AbstractFactoryMode/FactoryMethod/SQLServerFactory.java
@@ -1,11 +1,18 @@
 package AbstractFactoryMode.FactoryMethod;
 
+import AbstractFactoryMode.Bean.IDepartment;
 import AbstractFactoryMode.Bean.IUser;
+import AbstractFactoryMode.Bean.SQLServerDept;
 import AbstractFactoryMode.Bean.SQLServerUser;
 
 public class SQLServerFactory implements IFactory {
     @Override
     public IUser CreateUser() {
         return new SQLServerUser();
+    }
+
+    @Override
+    public IDepartment CreateDepartment() {
+        return new SQLServerDept();
     }
 }
